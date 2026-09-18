98cb9adf64e75bc71df8f99ab0454d3541d473e7
Mukul <mukulyadav2302@gmail.com>
2026-09-18 14:13:12 +0545

    fix: replaced .config() to defineConfig() in eslint.config.mjs and removed duplicate lines in gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ["**/dist/**", "**/build/**", "**/node_modules/**"],
   },
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
 );
